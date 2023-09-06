1db16ae28cc93a445f04fe42c591855ccf8c6dd2
Tim Zimmermann <tim@linux4.de>
2023-09-06 06:38:44 +0200

    pi4: Switch to in-tree u-boot env source file
    Change-Id: I46513fe26506c9cde178ae6042da2c9d23b65d23

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -177,6 +177,7 @@ BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 ## U-Boot
 TARGET_U_BOOT_CONFIG := rpi_4_defconfig
 TARGET_U_BOOT_CONFIG_FRAGMENT := $(DEVICE_PATH)/configs/bootloader/rpi_4_android.config
+TARGET_U_BOOT_ENV := $(DEVICE_PATH)/configs/bootloader/env.txt
 TARGET_U_BOOT_SOURCE := external/raspberrypi/u-boot
 
 ## Verified Boot
